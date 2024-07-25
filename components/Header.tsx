94c14c14d0c3c55c1a3ead7a1721ebe77ad74b92
ranahunter10 <ashishrana1619@gmail.com>
2024-07-25 23:32:18 +0530

    project uploaded to git repo

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = ({ children, className }: HeaderProps) => {
     <div className={cn("header", className)}>
       <Link href='/' className="md:flex-1">
         <Image 
-          src="/assets/icons/logo.svg"
+          src="/assets/icons/sonu.png"
           alt="Logo with name"
           width={120}
           height={32}
